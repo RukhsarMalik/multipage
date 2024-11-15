f231925ff8a4c99caa6e3a95143ad4338b93e978
RukhsarMalik <103738665+RukhsarMalik@users.noreply.github.com>
2024-11-15 21:58:36 +0500

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,9 @@
 import BooksDisplay from "./components/BooksDisplay";
 import Intro from "./components/intro";
-import Navbar from "./components/navbar";
 
 export default function Home() {
   return (
     <div>
-      <Navbar/>
       <Intro/>
       <BooksDisplay/>
     </div>
