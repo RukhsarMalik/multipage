689ddcf18ce2629e18d07a009d0d5e89ae02ffd4
RukhsarMalik <103738665+RukhsarMalik@users.noreply.github.com>
2024-11-15 21:31:01 +0500

    Create intro.tsx

---
+++ src/app/components/intro.tsx
@@ -0,0 +1,17 @@
+// components/Intro.tsx
+const Intro = () => {
+    return (
+      <section className="bg-gray-100 py-16">
+        <div className="max-w-7xl mx-auto text-center px-4">
+          <h2 className="text-3xl font-bold text-gray-800 mb-4">Welcome to Online Book Reading</h2>
+          <p className="text-lg text-gray-600 mb-8">
+            Discover an extensive collection of books in various genres. Whether you are looking for the latest bestsellers, classic novels, or informative non-fiction, we have something for everyone. Start reading today and dive into the world of literature from the comfort of your home.
+          </p>
+          
+        </div>
+      </section>
+    );
+  };
+  
+  export default Intro;
+  
